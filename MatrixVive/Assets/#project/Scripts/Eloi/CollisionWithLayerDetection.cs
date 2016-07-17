4bc648e952d1bdc70f63e561ecdeea62961c7b04
JamsCenter <Jams.Center@gmail.com>
2016-07-17 15:33:33 +0200

    Commit All

---
+++ MatrixVive/Assets/#project/Scripts/Eloi/CollisionWithLayerDetection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class CollisionWithLayerDetection : MonoBehaviour {
+
+    public LayerMask _layerAllowAsCollision;
+
+
+    public delegate void OnHitDetected(Transform byWhat, Vector3 where);
+    public OnHitDetected _onHitten;
+    public UnityEvent _onHittendEvent;
+    
+
+    void OnTriggerEnter(Collider col) {
+
+        if (IsInLayerMask(col.gameObject, _layerAllowAsCollision)) {
+            
+            if (_onHitten != null)
+                _onHitten(col.transform, col.ClosestPointOnBounds(transform.position));
+            _onHittendEvent.Invoke();
+
+        }
+
+    }
+
+    public static bool IsInLayerMask(GameObject obj, LayerMask mask)
+    {
+        return ((mask.value & (1 << obj.layer)) > 0);
+    }
+
+}
---
+++ MatrixVive/Assets/#project/Scripts/Eloi/Destroyable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyable : MonoBehaviour {
+
+    public Transform _affected;
+    public void Reset() {
+        if(!_affected) _affected = this.transform; }
+    public void Destroy() {
+
+        GameObject.Destroy(_affected.gameObject);
+    }
+}
