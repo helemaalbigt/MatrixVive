d7c60250a35f6c298c0eedf56cad8487748413d8
Thomas VB <van.bouwel.thomas@gmail.com>
2016-07-17 16:26:41 +0200

    Stuff

---
+++ MatrixVive/Assets/#project/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public static int currentScore = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	private IEnumerator GameLoop(){
+
+	}
+
+	private IEnumerator PlayersTakePosition(){
+
+	}
+
+	private IEnumeratorRoundPlaying(){
+
+	}
+}
--- MatrixVive/Assets/#project/Scripts/GunController.cs
+++ MatrixVive/Assets/#project/Scripts/GunController.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class GunController : MonoBehaviour {
 
+	public float _ReloadTime;
+
+	private bool _ReadyToFire = true;
+	private bool _Recharging = false;
+	private int _Bullets = 6;
+
 	public WandController _Controller;
 	public Transform _GunTip;
 	public GameObject _BulletPrefab;
@@ -14,12 +20,64 @@ public class GunController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(_Controller.triggerDown)
-			FireBullet ();
+		if (_Controller.triggerDown)
+		{
+			if (_ReadyToFire && _Bullets > 0)
+			{
+				_Bullets--;
+				FireBullet ();
+			} else
+			{
+				StartCoroutine (EmptyRumble ());
+				_ReadyToFire = false;
+				if (!_Recharging)
+				{
+					_Recharging = true;
+					StartCoroutine (Recharge ());
+				}
+			}
+		}
 	}
 
 	private void FireBullet(){
+		//instantiate bullet
 		GameObject createdBullet = Instantiate (_BulletPrefab, _GunTip.position, Quaternion.identity) as GameObject;
 		createdBullet.transform.up = _GunTip.transform.forward;
+
+		//vibrate controller
+		StartCoroutine(FireRumble(10, 0.15f));
 	}
+		
+	IEnumerator Recharge(){
+		yield return new WaitForSeconds (_ReloadTime);
+
+		StartCoroutine (RechargeRumble ());
+		_Bullets = 6;
+		_ReadyToFire = true;
+		_Recharging = false;
+	}
+
+	IEnumerator RechargeRumble(){
+		_Controller.Vibrate (2000);
+		yield return new WaitForSeconds (0.1f);
+		_Controller.Vibrate (2000);
+	}
+
+	IEnumerator EmptyRumble(){
+		_Controller.Vibrate (2000);
+		yield return new WaitForSeconds (0.1f);
+	}
+
+	IEnumerator FireRumble(int rumbles, float duration){
+		float timePassed = 0;
+
+		while (timePassed < duration)
+		{
+			timePassed += ( duration / rumbles );
+
+			_Controller.Vibrate (2000);
+			yield return new WaitForSeconds (duration / rumbles);
+		}
+	}
+
 }
