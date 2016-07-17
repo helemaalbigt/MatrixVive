d43e682dc7e0e60b6529027bcff41c6952a2f771
Thomas VB <van.bouwel.thomas@gmail.com>
2016-07-17 13:44:03 +0200

    Elements

---
+++ MatrixVive/Assets/#project/Scripts/GunController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour {
+
+	public WandController _Controller;
+	public Transform _GunTip;
+	public GameObject _BulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(_Controller.triggerDown)
+			FireBullet ();
+	}
+
+	private void FireBullet(){
+		GameObject createdBullet = Instantiate (_BulletPrefab, _GunTip.position, Quaternion.identity) as GameObject;
+		createdBullet.transform.up = _GunTip.transform.forward;
+	}
+}
